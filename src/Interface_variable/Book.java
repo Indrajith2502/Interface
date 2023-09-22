a63bcf7dc0c52fdf85b11e2091d6744974b4d6ae
Indrajith2502 <144647834+Indrajith2502@users.noreply.github.com>
2023-09-22 23:34:42 +0530

    Interface variable

---
+++ src/Interface_variable/Book.java
@@ -0,0 +1,12 @@
+package Interface_variable;
+
+public interface Book {
+
+	int a = 10;
+	static String s = " blue" ;
+	final char ch ='b' ;
+	
+
+	
+
+}
---
+++ src/Interface_variable/Pen.java
@@ -0,0 +1,16 @@
+package Interface_variable;
+
+public class Pen implements Book{
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Pen p = new Pen();
+      System.out.println(p.a);
+      System.out.println(p.s);
+      System.out.println(p.ch);
+      }
+
+}
